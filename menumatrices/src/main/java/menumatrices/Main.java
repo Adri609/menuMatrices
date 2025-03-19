81a9086a78f4814ee3129f13e5e23c784c019a87
Adri609 <adri.bog3@gmail.com>
2025-03-19 14:25:01 +0100

    Avances
    correción de funciones y nuevas funciones añadidas

--- menumatrices/src/main/java/menumatrices/Main.java
+++ menumatrices/src/main/java/menumatrices/Main.java
@@ -1,9 +1,45 @@
 package menumatrices;
 
+import java.util.Scanner;
+
 public class Main {
+    
+    private static Scanner read = new Scanner(System.in);
+    private static int filas, columnas, cont = 0;
+    private static int[][] matriz1;
+    private static int[][] matriz2;
+
+    // Función para rellenar matrices
+    public static void rellenar (int[][] matriz) {
+
+        System.out.println("Introduce el número de filas de la matriz:");
+        filas = read.nextInt();
+        System.out.println("Introduce el número de columnas de la matriz:");
+        columnas = read.nextInt();
+
+        // Iniciamos la matriz con el número de filas y columnas declarado y la rellenamos con valores pedidos al usuario
+        matriz = new int[filas][columnas];
+            for(int i = 0; i < filas; i++){
+                for (int j = 0; j < columnas; j++){
+                    cont++;
+                    System.out.println("Introduce el valor "+ (cont + 1) + " de la matriz");
+                    matriz[i][j] = read.nextInt();
+                }
+            }
+    }
+
+    // Función que imprime matrices   
+    public static void imprimir (int[][] imprimir){
+        for (int i = 0; i < imprimir.length; i++) {
+            for (int j = 0; j < imprimir[0].length; j++){
+                System.out.print(imprimir[i][j]+" ");
+            }
+            System.out.println("");   
+        }
+    }
 
     // Función para multiplicar matrices
-    public static int[][] multiplicarMatriz (int[][] matriz1, int [][]matriz2) {
+    public static int[][] multiplicarMatriz (int[][] matriz1, int[][] matriz2) {
         
         // Compruebo que las matrices se puedan multiplicar
         if (matriz1[0].length != matriz2.length) {
@@ -66,29 +102,57 @@ public class Main {
     }
 
     // Función para comprobar que una matriz sea mágica
-    public static boolean esMatrizMagica(int[][] matriz) {
+    public static boolean magica(int[][] matriz) {
         
-        // Calculo la suma de la primera fila
-        int sumaFila1 = matriz[0][0] + matriz[0][1];
-        
-        // Calculo la suma de la primera columna
-        int sumaColumna1 = matriz[0][0] + matriz[1][0];
+        int sumaColumnas[] = new int[matriz[0].length];
+        int sumaFilas = 0;
 
-        // Verifico que la suma de la primera fila y la primera columna sean iguales
-        if (sumaFila1 != sumaColumna1) {
-            return false;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                sumaFilas += matriz[i][j];
+                sumaColumnas[j] += matriz[i][j];
+            }
         }
-
-        // Comprobamos la segunda fila y columna
-        int sumaFila2 = matriz[1][0] + matriz[1][1];
-        int sumaColumna2 = matriz[0][1] + matriz[1][1];
-
-        // Comprobamos si todas las sumas son iguales
-        return sumaFila1 == sumaFila2 && sumaFila1 == sumaColumna2;
-
+       
     }
+
+    // Función para iniciar matrices
 
     public static void main(String[] args) {
         
+        int opcion; 
+        // Relleno e imprimo en pantalla la matriz 1
+        rellenar(matriz1);
+        imprimir(matriz1);
+
+        // Relleno e impmrimo en pantalla la matriz 2
+        rellenar(matriz2);
+        imprimir(matriz2);
+
+        do {
+            System.out.println("Elije una opción: /n");
+            opcion = read.nextInt();
+            System.out.println("1- Sumar matrices.");
+            System.out.println("2- Restar matrices.");
+            System.out.println("3- Multiplicar matrices.");
+            System.out.println("4- Comprobar si una matir es mágica");
+            System.out.println("5- Salir");
+
+        } while (opcion > 0 || opcion < 5);
+
+        switch(opcion) {
+            case 1:
+            sumarMatriz(matriz1, matriz2);
+            break;
+            case 2:
+            restarMatriz(matriz1, matriz2);
+            break;
+            case 3:
+            multiplicarMatriz(matriz1, matriz2);
+            break;
+            case 4:
+            System.out.println("De que matriz quieres ");
+        }
     }
+
 }
